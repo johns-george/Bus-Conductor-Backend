e0f7dd11f895d91729d9b3f4bfd14ba170b288eb
azmisal <azmisaleem96@gmail.com>
2026-04-01 03:45:20 +0530

    add: DB Connection logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from src.core.database import engine
+from sqlalchemy import text
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "API running 🚀"}
+
+
+@app.get("/test-db")
+def test_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        return {"status": "connected", "result": [row[0] for row in result]}
