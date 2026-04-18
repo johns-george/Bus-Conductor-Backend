ba2b02637145292007e2084f6278ab7424ee83cd
johns-george <johnsgeorge14@gmail.com>
2026-04-18 12:47:28 +0530

    add:delete ticket api

--- src/api/routes/ticket.py
+++ src/api/routes/ticket.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.core.database import get_db
-from src.services.ticket_service import get_all_tickets, get_ticket, create_ticket, update_ticket
+from src.services.ticket_service import get_all_tickets, get_ticket, create_ticket, update_ticket, cancel_ticket
 from src.schemas.ticket_response import TicketResponse
 from src.models.ticket import TicketUpdate
 from typing import List
@@ -27,14 +27,18 @@ def  book_ticket(
         destination_stop_id
     )
 
-@router.get("/get-all-tickets/{user_id}" response_model=List[TicketResponse])
+@router.get("/get-all-tickets/{user_id}", response_model=List[TicketResponse])
 def fetch_all_tickets(user_id: int,db: Session = Depends(get_db)):
     return get_all_tickets(db,user_id)
 
-@router.get("/get-ticket-details/{ticket_id}" response_model=TicketResponse)
+@router.get("/get-ticket-details/{ticket_id}", response_model=TicketResponse)
 def fetch_ticket(ticket_id: int, db: Session = Depends(get_db)):
     return get_ticket(db, ticket_id)
 
 @router.put("/update-ticket/{ticket_id}")
 def update_ticket_details(ticket_id: int, ticket_data: TicketUpdate,db: Session = Depends(get_db)):
-    return update_ticket(db, ticket_id, ticket_data) 
+    return update_ticket(db, ticket_id, ticket_data)
+
+@router.delete("/cancel-ticket/{ticket_id}")
+def cancel_ticket(ticket_id: int, db: Session = Depends(get_db)):
+    return cancel_ticket(db, ticket_id)
--- src/services/ticket_service.py
+++ src/services/ticket_service.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import Session
 from src.models.ticket import ticket
-from src.models.bus_trip import Trip
 from datetime import datetime
 
 def create_ticket(
@@ -11,19 +10,19 @@ def create_ticket(
     source_stop_id: int,
     destination_stop_id: int
 ):
-ticket = Ticket(
+    new_ticket = ticket(
         user_id=user_id,
         trip_id=trip_id,
         seat_id=seat_id,
         source_stop_id=source_stop_id,
         destination_stop_id=destination_stop_id,
-        booking_time = datetime.utcnow()
+        booking_time = datetime.utcnow(),
         status = "CONFIRMED"
     )
-    db.add(ticket)
+    db.add(new_ticket)
     db.commit()
-    db.refresh(ticket)
-    return ticket
+    db.refresh(new_ticket)
+    return new_ticket
 
 def get_all_tickets(db: Session, user_id: int):
     return db.query(ticket).filter(ticket.user_id == user_id).order_by(ticket.id).all()
@@ -38,6 +37,16 @@ def update_ticket(db: Session, ticket_id: int, ticket_data):
     update_data = ticket_data.dict(exclude_unset=True)
     for key, value in update_data.items():
         setattr(ticket, key, value)
+    ticket.booking_time = datetime.utcnow()
+    db.commit()
+    db.refresh(ticket)
+    return ticket
+
+def cancel_ticket(db: Session, ticket_id: int):
+    ticket = db.query(ticket).filter(ticket.id == ticket_id).first()
+    if not ticket:
+        return {"error": "Ticket not found"}
+    ticket.status = "CANCELLED"
     ticket.booking_time = datetime.utcnow()
     db.commit()
     db.refresh(ticket)
