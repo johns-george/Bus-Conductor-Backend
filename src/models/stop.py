95c9c48f0fe3ae2657759e75e0a50debb6cc5ae3
azmisal <azmisaleem96@gmail.com>
2026-04-19 18:31:21 +0530

    add: get_stop_by_keyword api added with all files

--- src/models/stop.py
+++ src/models/stop.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, Index
 from sqlalchemy.orm import relationship
 from src.core.database import Base
 
@@ -7,11 +7,23 @@ class Stop(Base):
 
     id = Column(BigInteger, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
-    address = Column(String(255))
-
+    city = Column(String(100))
+    district = Column(String(100))
     # Graph relationships
     outgoing_edges = relationship("Edge", foreign_keys="Edge.from_stop", back_populates="from_stop_rel")
     incoming_edges = relationship("Edge", foreign_keys="Edge.to_stop", back_populates="to_stop_rel")
 
     # Route relationships
     route_stops = relationship("RouteStop", back_populates="stop")
+    
+    #Indexing for faster search by name
+    __table_args__ = (
+        Index("idx_stop_city", "city"),
+        Index("idx_stop_district", "district"),
+        Index(
+            "idx_stop_name_trgm",
+            "name",
+            postgresql_using="gin",
+            postgresql_ops={"name": "gin_trgm_ops"}
+        ),
+    )
--- src/services/stop_service.py
+++ src/services/stop_service.py
@@ -1,6 +1,21 @@
 from sqlalchemy.orm import Session
 from src.models.stop import Stop
-
+from src.dao.stop_dao import fetch_stops_by_keyword
+from src.schemas.stop_schema import StopResponse
 
 def get_all_stops(db: Session):
     return db.query(Stop).order_by(Stop.id).all()
+
+def get_stops_by_keyword_city_district(db: Session, keyword: str, city: str, district: str):
+    
+    stops = fetch_stops_by_keyword(db, keyword, city, district)
+
+    return [
+        StopResponse(
+            id=stop.id,
+            name=stop.name,
+            city=stop.city,
+            district=stop.district
+        )
+        for stop in stops
+    ]
